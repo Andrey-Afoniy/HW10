00c8d268f6b6dc49dc0e7a06b99b0ea6614bdd46
Andrey-Afoniy <mr.knop2015@yandex.ru>
2025-06-28 14:59:43 +0400

    HW10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Задание 1");
+        String firstname = "Ivan";
+        String middlename = "Ivanovich";
+        String lastname = "Ivanov";
+        String fullName = firstname + " " + middlename + " " + lastname;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задание 2");
+        System.out.println("Ф. И. О. сотрудника — " + fullName.toUpperCase());
+
+        System.out.println("Задание 3");
+        String fullName1 = "Иванов Семён Семёнович";
+        String correctedFullName1 = fullName1.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + correctedFullName1);
     }
 }
+
+
